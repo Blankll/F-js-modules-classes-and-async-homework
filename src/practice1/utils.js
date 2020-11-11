2676b7193244c86be2045f0b21499f792d5b7dea
Lisheng Zi <lisheng.zi@thoughtworks.com>
2020-11-11 23:28:27 +0800

    ok

--- src/practice1/utils.js
+++ src/practice1/utils.js
@@ -1,1 +1,3 @@
-const getURL
+export default (address, port, path) => {
+    return `${address}:${port}${path}`
+}
--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,23 @@
-export const getPoetry
+import axios from 'axios'
+import { get } from 'fetch-mock';
+
+// export const getPoetry = async () => {
+//     try{
+//     const response = await fetch('https://v1.jinrishici.com/all.json', { method: 'GET' });
+//     const {origin, author, content} = await response.json();
+//     return [origin, author, content];
+//     } catch (e) { 
+//         console.log(e)
+//         return []
+//     }
+// }
+
+export const getPoetry = () => {
+    return fetch('https://v1.jinrishici.com/all.json', { method: 'GET' })
+      .then(data => data.json())
+      .then(data => [data.origin, data.author, data.content])
+      .catch ((e) => { 
+        console.log(e)
+        return []
+    });
+}
